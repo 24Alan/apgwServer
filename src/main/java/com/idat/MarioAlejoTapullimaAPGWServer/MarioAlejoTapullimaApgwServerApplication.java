f3cacf41e2f0bff09d22d99f2f06c469b5089025
24Alan <86678009+24Alan@users.noreply.github.com>
2022-06-03 23:54:51 -0500

    Add files via upload

---
+++ src/main/java/com/idat/MarioAlejoTapullimaAPGWServer/MarioAlejoTapullimaApgwServerApplication.java
@@ -0,0 +1,13 @@
+package com.idat.MarioAlejoTapullimaAPGWServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarioAlejoTapullimaApgwServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarioAlejoTapullimaApgwServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/idat/MarioAlejoTapullimaAPGWServer/MarioAlejoTapullimaApgwServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idat.MarioAlejoTapullimaAPGWServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarioAlejoTapullimaApgwServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
